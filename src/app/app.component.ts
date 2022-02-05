5ef1fdb9d54d6e980fdbaae2f6382185cff8f5db
Geoff Fox <fox.geoffrey@gmail.com>
2022-02-05 13:18:05 -0800

    Example #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
+import { catchError, map, skip, take, tap } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-ex1';
+
+  ngOnInit() {
+    // of(2, 4, 6, 8, 5).subscribe(console.log);
+
+    // Example #2
+    from([10, 20, 30, 5])
+      .pipe(
+        tap(item => console.log(`Org: ${item}`)),
+        map(item => item * 2),
+        map(item => item - 10),
+        map(item => {
+          if (item === 0) {
+            throw new Error(`Zero detected - stream stopped!`)
+          }
+          return item;
+        }),
+        take(5)
+      ).subscribe(
+        item => console.log(`result: ${item}`),
+        err => console.error(`eror ${err}`,
+          () => console.log(`completed!`))
+      );
+  }
+
 }
